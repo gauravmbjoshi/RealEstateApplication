4ea61701b03b0bc5bf71db492d60ba6983a058b5
Gaurav Joshi <79218309+gauravmbjoshi@users.noreply.github.com>
2023-10-28 09:07:05 +0530

    change in auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,5 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -8,7 +7,6 @@ export const signup = async (req, res, next) => {
   try {
       await newUser.save();
       res.status(201).json('User created successfully!');
-    
   } catch (error) {
     next(error);
   }
